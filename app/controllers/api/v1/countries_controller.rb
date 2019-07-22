babd29b173b02c662dafcd3eb870b26be2298784
Milan <milan.dhar@gmail.com>
2019-07-22 14:31:35 -0700

    fixed get projects

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -18,6 +18,7 @@ class Api::V1::CountriesController < ApplicationController
 
   def get_projects
     country = Country.find_by(iso3166CountryCode: params["countryCode"])
+    byebug
     country_projects = country.projects
     render json: country_projects
   end
