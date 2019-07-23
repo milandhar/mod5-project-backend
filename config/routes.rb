04f9ad3ce80cf3d47926d740a7767f9273af0f71
Milan <milan.dhar@gmail.com>
2019-07-23 15:35:34 -0700

    added user update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :index]
+      resources :users, only: [:create, :index, :update]
       resources :countries, only: [:create, :index]
       resources :projects, only: [:create, :index]
       resources :themes, only: [:create, :index]
