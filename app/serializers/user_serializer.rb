6b79fd1e5bd90a0edb45096923d6a216979bac59
Milan <milan.dhar@gmail.com>
2019-07-23 10:58:49 -0700

    about to reset user table

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :email_address, :first_name, :last_name, :theme_interest_1, :theme_interest_2, :theme_interest_3, :projects
+  attributes :id, :username, :password_digest, :email_address, :first_name, :last_name, :theme1, :theme2, :theme3, :projects
 end
