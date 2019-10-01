06f3d5f621f560bb1c4613e385bee62bc4a55d17
Milan <milan.dhar@gmail.com>
2019-10-01 16:35:43 -0700

    added donation options to serializer of project

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,4 @@
 class ProjectSerializer < ActiveModel::Serializer
   #With joins attributes :id, :title, :status, :funding, :goal, :project_donation_options, :country, :theme, :organization
-  attributes :id, :country, :activities, :theme, :project_link, :organization, :gg_project_id, :gg_organization_id, :latitude, :longitude, :title, :status, :funding, :goal, :long_term_impact, :summary, :image_url
+  attributes :id, :country, :activities, :theme, :project_link, :organization, :gg_project_id, :gg_organization_id, :latitude, :longitude, :title, :status, :funding, :goal, :long_term_impact, :summary, :image_url, :donation_descriptions, :donation_amounts
 end
