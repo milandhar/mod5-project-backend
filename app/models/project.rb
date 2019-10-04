61140e668b5e95b3cc4f6a83aad906d02e0ad6fb
Milan <milan.dhar@gmail.com>
2019-10-04 13:37:27 -0700

    added check of org number

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,75 +21,76 @@ class Project < ApplicationRecord
 
   def self.fetch(params)
     #Add a check to see if Project count < 7000. Added bc of Heroku 10k row limit
-    if Project.all.count < 7000
-    @projects = []
-    json = self.queryActiveProjects(params)
-    NormalizeCountry.to = :alpha3
-    json.first[1]["project"].each do |project|
-      if(project["organization"] &&
-              project["organization"]["themes"] &&
-              project["organization"]["countries"])
+    if (Project.all.count + Organization.all.count) < 8000
+      @projects = []
+      json = self.queryActiveProjects(params)
+      NormalizeCountry.to = :alpha3
+      json.first[1]["project"].each do |project|
+        if(project["organization"] &&
+                project["organization"]["themes"] &&
+                project["organization"]["countries"])
 
-        @project = Project.find_or_create_by(
-          funding: project["funding"],
-          goal: project["goal"],
-          image_url: project["imageLink"],
-          latitude: project["latitude"],
-          longitude: project["longitude"],
-          long_term_impact: project["longTermImpact"],
-          activities: project["activities"],
-          need: project["need"],
-          gg_organization_id: project["organization"]["id"],
-          gg_project_id: project["id"],
-          project_link: project["projectLink"],
-          status: project["status"],
-          summary: project["summary"],
-          theme_str_id: project["themeName"],
-          title: project["title"]
-        )
+          @project = Project.find_or_create_by(
+            funding: project["funding"],
+            goal: project["goal"],
+            image_url: project["imageLink"],
+            latitude: project["latitude"],
+            longitude: project["longitude"],
+            long_term_impact: project["longTermImpact"],
+            activities: project["activities"],
+            need: project["need"],
+            gg_organization_id: project["organization"]["id"],
+            gg_project_id: project["id"],
+            project_link: project["projectLink"],
+            status: project["status"],
+            summary: project["summary"],
+            theme_str_id: project["themeName"],
+            title: project["title"]
+          )
 
-        # add donation options to @project
-        if project["donationOptions"]
-          project["donationOptions"]["donationOption"].each do |option|
-            if option["description"] && option["amount"]
-              @project.donation_descriptions.push(option["description"])
-              @project.donation_amounts.push(option["amount"])
+          # add donation options to @project
+          if project["donationOptions"]
+            project["donationOptions"]["donationOption"].each do |option|
+              if option["description"] && option["amount"]
+                @project.donation_descriptions.push(option["description"])
+                @project.donation_amounts.push(option["amount"])
+              end
             end
           end
-        end
 
-        if (!Organization.find_by(Gg_organization_id: project["organization"]["id"]))
-          #find or create organization here
-          @organization = Organization.create(
-            Gg_organization_id: project["organization"]["id"],
-            city: project["organization"]["city"],
-            country: project["organization"]["country"],
-            mission: project["organization"]["mission"],
-            name: project["organization"]["name"],
-            url: project["organization"]["url"]
-          )
-          @project.organization = @organization
-          @organization.save
-        else
-          @organization = Organization.find_by(Gg_organization_id: project["organization"]["id"])
-          @project.organization = @organization
+          if (!Organization.find_by(Gg_organization_id: project["organization"]["id"]))
+            #find or create organization here
+            @organization = Organization.create(
+              Gg_organization_id: project["organization"]["id"],
+              city: project["organization"]["city"],
+              country: project["organization"]["country"],
+              mission: project["organization"]["mission"],
+              name: project["organization"]["name"],
+              url: project["organization"]["url"]
+            )
+            @project.organization = @organization
+            @organization.save
+          else
+            @organization = Organization.find_by(Gg_organization_id: project["organization"]["id"])
+            @project.organization = @organization
+          end
+          @theme = Theme.find_or_create_by(name: project["themeName"])
+          @theme.save
+          @project.theme = @theme
+          @country = Country.find_by(name: project["country"])
+          @country.save
+          @project.country = @country
+          @project.save
+          @projects << @project
         end
-        @theme = Theme.find_or_create_by(name: project["themeName"])
-        @theme.save
-        @project.theme = @theme
-        @country = Country.find_by(name: project["country"])
-        @country.save
-        @project.country = @country
-        @project.save
-        @projects << @project
+      end
+      # Check to see if there are still more projects in the GlobalGiving API
+      # If so --> output the next project ID to the console, and fetch the next list of projects from the API using the next projectId
+      if json["projects"]["hasNext"]
+        params[:nextProject] = json["projects"]["nextProjectId"]
+        puts json["projects"]["nextProjectId"]
+        self.fetch(params)
       end
     end
-    if json["projects"]["hasNext"]
-      params[:nextProject] = json["projects"]["nextProjectId"]
-      puts json["projects"]["nextProjectId"]
-      self.fetch(params)
-    end
-    # render json: {"has_next": json["projects"]["hasNext"], "nextProjectId": json["projects"]["nextProjectId"], "projects": @projects}
   end
-end
 end
