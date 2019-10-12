9c8ccf996c9248720a970c2f87e427c9893018bf
Milan <milan.dhar@gmail.com>
2019-10-12 16:14:08 -0700

    added test_helper, system, controllers folder to test

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a project without a title" do
+    project = Project.new
+    assert_not project.save
+  end
 end
