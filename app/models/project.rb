cb1fc9694d4e24c1ae587adf74da33f881d5b53d
Milan <milan.dhar@gmail.com>
2019-10-12 19:26:30 -0700

    added test for image url present

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,7 @@ class Project < ApplicationRecord
   has_many :user_starred_projects
   has_many :users, through: :user_starred_projects
   validates :title, presence: true
+  validates :image_url, presence: true
 
 
   def self.queryActiveProjects(params)
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class ProjectTest < ActiveSupport::TestCase
   test "should not save a project without a title" do
     project = Project.new
+    project.image_url = "www.google.com"
+    assert_not project.save, "Saved the project without a title"
+  end
+
+  test "should not save a project without an image url" do
+    project = Project.new
+    project.title = "Save Lives"
     assert_not project.save, "Saved the project without a title"
   end
 end
