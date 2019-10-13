0ce23189820d4cc6d00fecab9650de02930d17d3
Milan <milan.dhar@gmail.com>
2019-10-12 19:20:54 -0700

    added message to test

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class ProjectTest < ActiveSupport::TestCase
   test "should not save a project without a title" do
     project = Project.new
-    assert_not project.save
+    assert_not project.save, "Saved the project without a title"
   end
 end
