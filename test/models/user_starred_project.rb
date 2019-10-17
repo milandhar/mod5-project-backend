941a61fcfc3c1cb7f2a965469d9741e9738f469a
Milan <milan.dhar@gmail.com>
2019-10-16 18:53:58 -0700

    added user starred project test of duplicates

---
+++ test/models/user_starred_project.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserStarredProjectTest < ActiveSupport::TestCase
+  test "should prevent a user from starring multiple projects" do
+    user1 = User.create(username: "barryobama", password: "bo")
+    project1 = Project.new
+    project1.title = "Save Lives"
+    project1.theme_str_id = "env"
+    project1.image_url = "www.google.com/img"
+    project1.project_link = "https://www.globalgiving.org/projects/nourish-a-young-brain-protect-one-ancient-culture/"
+    project1.save
+    user_star_1 = UserStarredProject.create(project_id: project1.id, user_id: user1.id)
+    user_star_2 = UserStarredProject.new(project_id: project1.id, user_id: user1.id)
+    assert_not user_star_2.save, "user starred a duplicate project"
+  end
+end
