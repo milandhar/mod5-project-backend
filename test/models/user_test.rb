5d0747e9c285b7a4fc7c1549222204ac6c02469d
Milan <milan.dhar@gmail.com>
2019-10-16 18:39:14 -0700

    finished duplicate username test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "should not allow duplicate user names" do
-    user1 = User.new(username: "barryobama")
-    user2 = User.new(username: "barryobama")
+    user1 = User.create(username: "barryobama", password: "bo")
+    user2 = User.new(username: "barryobama", password: "michelle")
     user1.save
     assert_not user2.save, "Saved a duplicate user name"
   end
