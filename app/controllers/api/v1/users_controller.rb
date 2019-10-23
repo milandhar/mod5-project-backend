1a5d4f6d7f49873d00e6cc49771907fdaf19b39b
Milan <milan.dhar@gmail.com>
2019-10-23 14:49:27 -0700

    wrote auth test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -48,11 +48,24 @@ class Api::V1::UsersController < ApplicationController
     project_id = params[:project_id].to_i
     @user = User.find(params[:user_id])
     @user_projects = @user.projects
-    @user_projects.each do |project|
-      if project[:id] == project_id
-        starred = true
+
+    if @user_projects.length > 0
+        @user_projects.each do |project|
+          if project[:id] == project_id
+            starred = true
+            break
+          end
+        end
+      else
+      UserStarredProject.all.each do |star|
+        if project_id == star.project_id && @user.id == star.user_id
+          starred = true
+          break
+        end
       end
     end
+
+
 
     if starred
       render json: {status: 'Project is starred.'}
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -50,24 +50,29 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
     test "should verify that a project is starred" do
        User.delete_all
-       user = User.create(user_params[:user])
-       user.projects.push(project)
-       p project
-       p user.projects
+       user = User.new(user_params[:user])
+       user.projects << project
+       user.save
        post "/api/v1/check_star", params: {user_id: user.id, project_id: project.id}.to_json, headers: { "Content-Type": "application/json" }
        # message = JSON.parse(@response.message)
        status_hash = JSON.parse(@response.body)
        assert_equal 'Project is starred.', status_hash["status"]
    end
 
-   # test "should verify that a project is not starred" do
-   #    user = User.create(user_params[:user])
-   #    #didn't add the project to this user
-   #    post "/api/v1/check_star", params: {user_id: user.id, project_id: project.id}.to_json, headers: { "Content-Type": "application/json" }
-   #    # message = JSON.parse(@response.message)
-   #    status_hash = JSON.parse(@response.body)
-   #    assert_equal 'Project is not starred.', status_hash["status"]
-   # end
+   test "should verify that a project is not starred" do
+      user = User.create(user_params[:user])
+      #didn't add the project to this user
+      post "/api/v1/check_star", params: {user_id: user.id, project_id: project.id}.to_json, headers: { "Content-Type": "application/json" }
+      # message = JSON.parse(@response.message)
+      status_hash = JSON.parse(@response.body)
+      assert_equal 'Project is not starred.', status_hash["status"]
+   end
 
-    #test login using auth_controller
+   test "should update user" do
+      user = User.create(user_params[:user])
+      put "/api/v1/users/#{user.id}", params: {id: user.id, first_name: "Dylan"}.to_json, headers: { "Content-Type": "application/json" }
+      res = JSON.parse(@response.body)
+      assert_equal 'Dylan', res["first_name"]
+   end
+
 end
